6e8566b452182ff5a4ee121a55dd1bf63a62652d
Espoir Badohoun <espoir.badohoun@gmail.com>
2024-01-20 22:45:41 +0100

    fix: for every errors

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,8 +6,8 @@ from pathlib import Path
 THIS_DIR = Path(__file__).parent
 TESTS_DIR_PARENT = (THIS_DIR / "..").resolve()
 
-sys.path.insert(0,str(TESTS_DIR_PARENT))
+# ensure that `from tests ...` import statements work within the tests/ dir
+sys.path.insert(0, str(TESTS_DIR_PARENT))
 
-pytest_plugins = [
-    "tests.fixtures.project_dir"
-]
+# register fixtures so that they can be used in tests
+pytest_plugins = ["tests.fixtures.project_dir"]
--- tests/consts.py
+++
@@ -1,5 +0,0 @@
-
-from pathlib import Path
-
-THIS_DIR = Path(__file__).parent
-PROJECT_DIR = (THIS_DIR  / "../").resolve()
