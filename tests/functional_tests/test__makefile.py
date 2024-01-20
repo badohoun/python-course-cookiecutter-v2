f92fe766f917065e8d284d615b54ef6c958ce081
Espoir Badohoun <espoir.badohoun@gmail.com>
2024-01-20 21:24:34 +0100

    fix: remove non used import functions

--- tests/functional_tests/test__makefile.py
+++ tests/functional_tests/test__makefile.py
@@ -3,47 +3,13 @@
 import subprocess
 from pathlib import Path
 
-from tests.fixtures.project_dir import project_dir
 
-"""
-generate a project 
-linting a project 
-     generate project test: assert project_dir.exists()
-     test__linting_passes: make lint-ci
-     tests__tests_pass:
-          install all tests requirements 
-          build a wheel and execute tests against it
-"""
+def test__linting_passes(project_dir: Path):
+    """Validate that the templatized project has no auto-fixable linting issues."""
+    subprocess.run(["make", "lint-ci"], cwd=project_dir, check=True)
 
 
-def test__linting_passes(project_dir:Path):
-    subprocess.run(["make" , "lint-ci"], cwd=project_dir, check=True)
-
-
-def test__tests_pass(project_dir:Path):
+def test__tests_pass(project_dir: Path):
+    """Validate the the templatized tests pass when executed against a templatized project."""
     subprocess.run(["make", "install"], cwd=project_dir, check=True)
     subprocess.run(["make", "test-wheel-locally"], cwd=project_dir, check=True)
-
-def test__install_succeeds():
-    ...
-
-
-
-
-
-"""
-Setup
-1. generate a project using cookiecutter
-2. create a virtual environnement and install project dependencies 
-
-
-
-Tests:
-3. run tests
-4. run linting 
-
-
-Cleanup/Teardown
-5. remove virtual environnment 
-6. remove generated project 
-"""
--- tests/utils/project.py
+++ tests/utils/project.py
@@ -1,40 +1,37 @@
 """Functions for creating a cookiecut project to be used in tests."""
 
-
 import json
-import shutil
 import subprocess
 from copy import deepcopy
 from pathlib import Path
-from re import template
 from typing import Dict
-
-import pytest
 
 from tests.consts import PROJECT_DIR
 
 
 def initialize_git_repo(repo_dir: Path):
-    # git init 
+    """Run git commands to make a directory into a valid git repository."""
+    # git init
     subprocess.run(["git", "init"], cwd=repo_dir, check=True)
-    # commit the contents to the 'main' branch 
-    subprocess.run(["git", "branch",  "-M" ,  "main"], cwd=repo_dir, check=True)
-    subprocess.run(["git", "add",  "--all"], cwd=repo_dir, check=True)
-    subprocess.run(["git" , "commit" , "-m" , "'feat: initial commit by pytest'"], cwd=repo_dir, check=True)
-    
+    # commit the contents to the 'main' branch
+    subprocess.run(["git", "branch", "-M", "main"], cwd=repo_dir, check=True)
+    subprocess.run(["git", "add", "--all"], cwd=repo_dir, check=True)
+    subprocess.run(["git", "commit", "-m", "'feat: initial commit by pytest'"], cwd=repo_dir, check=True)
 
-def generate_project(template_values:Dict[str,str], test_session_id:str):
-    """                           ./tests/test__generate_project.py::test__can_generate_project 
-    
-    
-    execute: cookiecutter <template directory> ...
+
+def generate_project(template_values: Dict[str, str], test_session_id: str):
     """
-    template_values : Dict[str,str] = deepcopy(template_values)
-    cookiecutter_config = {
-        "default_context": template_values
-    }
-    cookie_cutter_config_fpath = PROJECT_DIR  /f"tests/cookiecutter-{test_session_id}.json"
-    cookie_cutter_config_fpath.write_text(json.dumps(cookiecutter_config))
+    Generate a boilerplate project that we can use to test the template.
+
+    :param template_values: jinja context used when populating template
+    :param test_session_id: potentially randomly generated string used to
+        ensure uniqueness of generated file names.
+    """
+    template_values: Dict[str, str] = deepcopy(template_values)
+    cookiecutter_config = {"default_context": template_values}
+    cookiecutter_config_fpath = PROJECT_DIR / f"tests/cookiecutter-{test_session_id}.json"
+    cookiecutter_config_fpath.write_text(json.dumps(cookiecutter_config))
+
     cmd = [
         "cookiecutter",
         str(PROJECT_DIR),
@@ -42,16 +39,11 @@ def generate_project(template_values:Dict[str,str], test_session_id:str):
         str(PROJECT_DIR / "sample"),
         "--no-input",
         "--config-file",
-        str(cookie_cutter_config_fpath),
-        "--verbose"
-    
+        str(cookiecutter_config_fpath),
+        "--verbose",
     ]
     print("COMMAND:", " ".join(cmd))
-    subprocess.run(
-        cmd,
-        check=True
-    )
+    subprocess.run(cmd, check=True)
 
     generated_repo_dir = PROJECT_DIR / "sample" / template_values["repo_name"]
-    return  generated_repo_dir
-            
+    return generated_repo_dir
