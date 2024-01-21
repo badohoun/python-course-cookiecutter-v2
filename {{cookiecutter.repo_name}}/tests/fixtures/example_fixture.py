7bc8a6b7c5ae6b344a259da3cfc954b5086070ae
Espoir Badohoun <espoir.badohoun@gmail.com>
2024-01-21 14:15:21 +0100

    fix: add docstring

--- {{cookiecutter.repo_name}}/tests/fixtures/example_fixture.py
+++ {{cookiecutter.repo_name}}/tests/fixtures/example_fixture.py
@@ -9,5 +9,6 @@ from tests.consts import PROJECT_DIR
 
 @pytest.fixture(scope="session")
 def test_session_id() -> str:
+    """Demonstrate how pytest fixtures are used."""
     test_session_id = str(PROJECT_DIR) + str(uuid4())[:6]
     return test_session_id
